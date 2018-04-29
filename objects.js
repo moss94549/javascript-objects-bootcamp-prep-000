a27ecbe89d50304d5937e51ff3285e50a2e42492
moss94549@gmail.com <moss94549@gmail.com>
2018-04-29 17:52:40 +0000

    automatically backed up by learn

---
+++ objects.js
@@ -0,0 +1,42 @@
+// Create Object
+meals = {breakfast:"bacon"};
+// or meals = new Object({breakfast:"bacon"});
+// or meals = new Object({}); -> empty Object
+
+const firstMeal = "breakfast";
+
+// meals  = {firstMeal:"oatmeal"}; -> firstMeal not breakfast
+// var meals = {[firstMeal]:"oatmeal"}; -> {breakfast:"oatmeal"}
+
+// Access values
+// meals.breakfast
+// meals[firstMeal]
+// meals["breakfast"]
+
+// Add key:value
+meals.snack = "yogurt";
+// or meals["snack"] = "bagel";
+
+// access: 
+meals["snack"]; // -> yogurt
+
+// pass reference (destructive)
+function destructivelyUpdateObject(obj, key, value)
+{
+    obj[key]=value;
+    return obj;
+}
+
+// Example: returns {eggs:3, flour:"3 cups"} also recipe
+const recipe = {egges:3};
+destructivelyUpdateObject(recipe, "flour", "3 cups");
+
+// pass by value (copy) 
+function updateObjectWithKeyValue(obj, key, value)
+{
+  // mergs everything into new Object
+  // in this case the empty object {}
+  return Object.assign( {}, obj, { [key]:value })
+}
+
+
